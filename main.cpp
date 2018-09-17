feaa55618820b5901f1339fec9a26c0fb28c498b
Jakub <43335861+jakob878@users.noreply.github.com>
2018-09-17 13:16:02 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float *liczba_do_testow = new float;
+    auto tablica_znakow[3] = {1,'a',5.5};
+
+    cout << *liczba_do_testow;
+}
